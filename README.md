eacf1cf42d9dac894193abea612cbefc3b05ef32
Najeeba <najeebaqarqin@gmail.com>
2024-08-19 13:44:42 +0430

    Maked the markdown text shorter Added more decription to readme file Added style to the markdown to look similar when the editor was empty

--- README.md
+++ README.md
@@ -4,12 +4,27 @@
 
 > This is the Markdown Previewer project created using React it's responsive and a freeCodeCamps frontend library project.
 
+
+#### Key features of this markdown previewer
+
+- Live rendering: Showes a real-time preview of the Markdown text as it is being written or edited.
+- Split View: Provides a side-by-side veiw with the Markdown editor on one side and the rendered preview on the other.
+- Basic formatting: Renders common Markdown syntax such as Heading, lists, bold, italics, links, and images.
+- Extended Syntax: Supports additional Markdown features link tables, code blocks, task lists, and footnotes.
+
+
+### During this project
+
 - I learned how to create a react app.
+- I learned how to make the components and combine them in to the main component.
+- I learned how to store data in a variable and how to return it in the display.
+- I learned how to use the react hooks.
 
 
 ## Demo 📸
 
 ![Demo](/src/images/Demo.png)
+- the live link : [Demo](https://profound-calfoutis-1fcdca.netlify.app/)
 
 
 ## Technologies and Tools Used 🛠️
@@ -20,6 +35,44 @@
 - css
 - Git
 - GitHu
+
+
+## Usage 🎯
+
+Explain how to use your project. Provide examples and instructions on how to run it, any command-line options, or any other relevant information.
+
+How to clone it:
+
+First, clone the repository to your local machine using Git. Open your terminal and run:
+
+```bash
+git clone git@github.com:Najeeba-Qarqin/MarkdownPreviewer.git
+```
+
+Change directory:
+
+Change the into the project directory:
+
+```bash
+cd MarkdownPreviewer
+```
+
+Install dependencies:
+
+Install the required dependencies using npm. This will download and set up all the packages needed for the project:
+
+```bash
+npm install
+```
+
+Run the project:
+
+start the live server to view the project in action. This command will launch a local development server:
+
+```bash
+npm start
+```
+
 
 
 ## Author 👩‍💻
--- src/Components/MarkdoqnPreveiwer.jsx
+++ src/Components/MarkdoqnPreveiwer.jsx
@@ -6,67 +6,11 @@ import remarkGfm from 'remark-gfm';
 const defaultMarkdown = `
 ## 1. Title Headings
 
-This is what headings will look like:
-
 # H1 Heading
 ## H2 Heading
-### H3 Heading
-#### H4 Heading
-##### H5 Heading
-###### H6 Heading
-
 ---
 
-## 2. Horizontal Rules
-
-Three different ways to write the rule element:
-
-___
----
-***
----
-
-## 3. Typographic replacements
-
-Enable typographer option to see result.
-
-(c) (C) (r) (R) (tm) (TM) (p) (P) +-
-
-test.. test... test..... test?..... test!....
-
-!!!!!! ???? ,,  -- ---
-
-"Smartypants, double quotes" and 'single quotes'
-
----
-
-## 3. Blocks of text
-
-This is what a couple of paragraphs will look like:
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae dui in velit bibendum aliquet nec vitae ex. Vivamus tincidunt porttitor est a lacinia. Nulla finibus tortor at nibh efficitur aliquam. Quisque tincidunt pharetra fermentum. Suspendisse et est eget dolor molestie tristique quis quis ex. Aenean ultricies sapien vel mi laoreet finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-
-Morbi egestas, enim a scelerisque lobortis, ipsum ex scelerisque velit, ut convallis massa urna non nisi. Duis tempor enim ut lectus porta semper. Vestibulum nec est vitae turpis mollis volutpat at sit amet magna. Aenean non sem euismod, sodales est a, iaculis magna. Praesent molestie ex sed erat bibendum, id blandit orci venenatis.
-
----
-
-## 4. Emphasis
-
-**This is bold text**
-
-__This is also bold text__
-
-*This is italic text*
-
-_This is also italic text_
-
-**_This is both bold and italic text_**
-
-~~This is strikethrough text~~
-
----
-
-## 5. Blockquotes
+## 2. Blockquotes
 
 This is how blockquotes will display:
 
@@ -76,7 +20,7 @@ This is how blockquotes will display:
 ---
 
 
-## 6. Lists
+## 3. Lists
 
 This is how unordered lists will look like:
 
@@ -85,23 +29,20 @@ This is how unordered lists will look like:
      - With different indentation levels.
         - That look like this.
 
-
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
 ---
 
-## 7. Links
+## 4. Links
 
 [This is what linked text looks like](http://github.com)
 
 [This is a link with a title](http://github.com "title text!")
 
-## 8. Code
 
+## 5. Code
 
 Inline code example: \`console.log('Hello, world!')\`
+
+Block code example:
 
 \`\`\`javascript
 // This is a code block
@@ -112,7 +53,7 @@ function greet() {
 
 ---
 
-## 9 Quote
+## 6 Quote
 
 > This is a block quote
 
@@ -120,20 +61,16 @@ This is **bolded text**
 
 A GIF image:
 
-*[GIF]: Graphics Interchange Format
-
 ![Daftpunktocat-Guy](https://octodex.github.com/images/daftpunktocat-guy.gif)
 
 A JPG image with a title:
-
-*[JPG]: Joint Photographic Experts Group
 
 ![Octocat De Los Muertos](https://octodex.github.com/images/octocat-de-los-muertos.jpg "Octocat De Los Muertos")
 
 Like links, images also have a footnote style syntax
 
 ---
-## 10 Line Break
+## 7 Line Break
 
 
 ---
